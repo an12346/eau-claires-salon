6156d8525ec88846ca0df919f5c9e5d7476a60f9
Anna Pittman <annaepittman@gmail.com>
2021-12-26 15:02:18 -0800

    create Client.cs and Stylist.cs folders

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,8 @@
+//MVC web application to help manage stylists and their clients
+//Should be able to add a list of stylists working at the salon
+//After clicking on a stylist, users should be able to see that stylist's clients 
+//Must be able to see a stylist's details 
+//Must have the ablity to add new stylists to the System
+//Must have the ablity to to add new clients to a specific stylist
+//Each client can only belong to one stylist 
+

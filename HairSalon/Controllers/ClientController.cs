9c92b236b3538ad77c8809093604f3821ba2ea70
Anna Pittman <annaepittman@gmail.com>
2021-12-26 17:14:35 -0800

    update stylist Index.cshtml view

---
+++ HairSalon/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Salon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Salon.Controllers
+{
+  public class ClientController : Controller
+  {
+    private readonly SalonContext _db;
+
+    public ClientController(SalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+  }
+}

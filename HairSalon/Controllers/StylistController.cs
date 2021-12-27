b25165e2135ecdc2f9ac605a2a5ecadb8386da45
Anna Pittman <annaepittman@gmail.com>
2021-12-26 16:39:16 -0800

    WIP

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Salon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Salon.Controllers
+{
+  public class StylistController : Controller
+  {
+    private readonly SalonContext _db;
+
+    public StylistController(SalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+  }
+}
